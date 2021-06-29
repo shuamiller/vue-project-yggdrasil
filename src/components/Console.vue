455df137cadec10499d1908728bdb48401f7d837
Shua Miller <thejoshmill@gmail.com>
2021-06-28 21:33:06 -0700

    Change #welcome-message font-family and change .game-text font-weight

--- src/components/Console.vue
+++ src/components/Console.vue
@@ -474,6 +474,10 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,200;0,400;0,900;1,200;1,400;1,900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;500;600;700&display=swap');
+
 #console {
   background-color: grey;
   margin: 1em;
@@ -552,6 +556,7 @@ export default {
 }
 
 #welcome-message {
+  font-family: "Fira Code", monospace;
   font-size: 1.5em;
   color: rgb(255, 227, 68);
 }
@@ -563,6 +568,7 @@ export default {
 
 .game-text {
   font-family: "Montserrat";
+  font-weight: 500;
   font-size: 1em;
   color: whitesmoke;
   line-height: 1.6;
