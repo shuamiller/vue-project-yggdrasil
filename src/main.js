4904d799ab5827dfb312692cbf609ade85e68daf
Shua Miller <thejoshmill@gmail.com>
2021-06-30 20:33:12 -0700

    Moved code from Console.vue to App.vue and comment out component in App file

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+
+import App from './App.vue';
+// import Console from "./components/Console.vue";
+
+// App.component('Console', Console)
 
 createApp(App).mount('#app')
